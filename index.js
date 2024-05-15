e2fbcb617a10f32ddaba14d2fe4274333bdb5553
shajid444 <mdshajidhasan76@gmail.com>
2024-05-15 19:01:06 +0600

    adding some comment

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// 
-// 
+
+
 //middleware
 app.use(cors());
 app.use(express.json());
@@ -130,6 +130,8 @@ async function run() {
 
         })
 
+        // added food api get method 
+        // ----------------------------------------------
         app.get('/addFood/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
@@ -193,6 +195,10 @@ async function run() {
     }
 }
 run().catch(console.dir);
+
+
+// api for connecting server
+// -------------------------------------------------
 
 
 app.get('/', (req, res) => {
